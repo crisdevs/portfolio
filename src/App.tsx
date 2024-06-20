ab75a077727da3e756a01d05416da20acba65874
Cristian Corrales <crisdevs95@gmail.com>
2024-06-20 12:59:14 -0400

    Added nav bar with social links and back to the top link

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,26 @@ import './styles/index.css'
 import Intro from './sections/Intro';
 import About from './sections/About';
 import Projects from './sections/Projects';
+import NavBar from './components/NavBar';
+import {useState} from "react";
 
 function App() {
-
+  const [showNav, setShowNav] = useState(false);
+  console.log("re-render");
+  window.onscroll = function(){
+    let Y = window.scrollY;
+    if(Y > 260){
+      setShowNav(true);
+    }
+    else{
+      setShowNav(false);
+    }
+    console.log(window.scrollY);
+  };
+  
   return (
     <div className = "bg-[#111111] text-white font-main flex flex-col items-center">
+    {showNav && <NavBar />}
     <Intro />
     <About />
     <Projects />
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaArrowAltCircleUp } from "react-icons/fa";
+
+
+
+export default function NavBar(){
+    return(
+        <div className = "fixed bg-[#191919] w-full top-0 flex justify-center">
+            <div className = "w-[85%] flex justify-evenly p-[10px]">
+            <a href = "#"><FaArrowAltCircleUp  className = "text-[35px]"/></a>
+            <a href = "https://github.com/crisdevs"><FaGithub className = "text-[35px]"/></a>
+            <a href = "https://www.linkedin.com/in/criscorr/"><FaLinkedin className = "text-[35px]"/></a>
+            <a href = "mailto:crisdevs95@gmail.com"><MdEmail className = "text-[35px]"/></a>
+            </div>
+        </div>
+    );
+}
