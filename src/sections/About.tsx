570c3cd870da6302bdf1850075edfca554483b80
Cristian Corrales <crisdevs95@gmail.com>
2024-07-17 14:27:14 -0400

    Updated about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -6,16 +6,15 @@ export default function About (){
             <section id= "about" className = "mt-[50px] lg:w-[90%] lg:h-screen">
                 {currentScreenSize.width < 1024 && <h2>ABOUT</h2>}
                 {currentScreenSize.width >= 1024 && <img className = "w-[75%] rounded-md mx-auto" src= {me}/>}
-                <p className = "mt-[20px]">Hello there friend! I am a software engineer based in New York with a background in IT support and product testing. 
-                    I am passionate about creating impactful and visually stunning web applications. My technical expertise includes JavaScript, React, TypeScript, 
-                    Redux, Styled-Components, Tailwind, and Git.
+                <p className = "mt-[20px]">Hello there, friend! I am a software engineer based in New York with a background in IT support and product testing.
+                     I am passionate about creating impactful and visually stunning web applications. My technical expertise includes JavaScript, React, TypeScript, 
+                     Redux, Styled-Components, Tailwind, and Git.
                    </p>
                    <p className = "mt-[20px]">
-                   Some cool things I have done are automate
-                   testing processes with the power of Javascript as a Senior Technician at Consumer Reports,
-                    Create a collaborative web application that help people of all skill level find their ideal plants, and create a personal 
-                   project to help me keep track of guitar tunings when I want to experiment on guitar.</p>
-                   <p className = "mt-[20px]">On my free time I love playing guitar and tackiling exciting challenges in the world of video games specifically the souls series!</p>
+                   Some cool things I have done include automating testing processes with the power of JavaScript as a Senior Technician at Consumer Reports, 
+                   creating a collaborative web application that helps people of all skill levels find their ideal plants, and developing a personal 
+                   project to track guitar tunings for experimenting on the guitar.</p>
+                   <p className = "mt-[20px]">In my free time, I love playing guitar and tackling exciting challenges in the world of video games, specifically the Souls series!</p>
                    
             </section>
     )
